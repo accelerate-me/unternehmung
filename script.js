1169b77ba7e607fe2b075b46ed3abfa2421b99fd
accelerate-me <60347026+accelerate-me@users.noreply.github.com>
2020-09-08 14:14:29 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function generator() {
+    // word lists
+    var firstPart = ["Spazieren", "Tanzen", "Urlaub planen", "Schwimmen", "Filmschauen", "Romantischen Abend"];
+   // var secondPart = ["the Coming Order", "the Relationship of Command", "Evil Androids", "Cybernetic Brain"];
+    // generate name
+    var name = firstPart[Math.floor(Math.random() * firstPart.length)];
+    //alert(name);
+    // remove name div if existing
+    if (document.getElementById("name")) {
+    document.getElementById("random-name").removeChild(document.getElementById("name"));
+    }
+    // append to placeholder div
+    var element = document.createElement("div");
+    element.setAttribute("id", "name");
+    element.appendChild(document.createTextNode(name));
+    document.getElementById("random-name").appendChild(element);
+    }
